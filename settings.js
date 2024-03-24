661038dfdd0acedea646f0aec91db1cd3e05d523
dark-sasiya <164601858+dark-sasiya@users.noreply.github.com>
2024-03-24 17:02:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,8 +18,8 @@ Creator : @darkmakerofc , @mr-nima-x
 module.exports = {
   OWNER_NUMBER: "94715166712",
   SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NAME: "𝐃𝐀𝐗𝐓𝐎𝐍 - 𝐈𝐃",
+  BOT_NAME : "𝐃𝐀𝐗𝐓𝐎𝐍 - 𝐈𝐃",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
